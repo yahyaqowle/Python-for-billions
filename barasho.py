92849364fd31a608116f63aaaeb6bb898df507ce
Qowle <79558044+yahyaqowle@users.noreply.github.com>
2023-01-24 09:06:55 +0300

    Day 2 learning

---
+++ barasho.py
@@ -0,0 +1,25 @@
+#Day 2 24 Jan
+# Python Syntax
+
+print("Hello, Fr oz")
+
+print("djfdf")
+
+# Python Indentation
+
+# problem 1
+if 5 > 2:
+    print("Five is greater than two!")
+
+# problem 2
+if 5 > 2:
+ print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+
+
+
+
+
+
+
