e5028a2f687a0d1db1571304d959092c330a0b68
Yahya Qowle <79558044+yahyaqowle@users.noreply.github.com>
2023-02-06 10:04:06 +0300

    06/02/2023

--- barasho.py
+++ barasho.py
@@ -1,122 +1,158 @@
+# """
+# ##Day 2 24 Jan
+# # Python Syntax
+
+# print("Hello, Fr oz")
+
+# print("djfdf")
+
+# # Python Indentation
+
+# # problem 1
+# if 5 > 2:
+#     print("Five is greater than two!")
+
+# # problem 2
+# if 5 > 2:
+#  print("Five is greater than two!")
+# if 5 > 2:
+#         print("Five is greater than two!")
+
+
+# # Day 3 26/01/23
+# # Comments
+
+# print("comment")
+# print("comment2") #this is an example of commments
+
+# """
+
+
+
+
+
+# ##Day 4 27/01/2023
+# """
+# # Variables
+
+# x = 5 # 5 is int (integer)
+# print(x)
+
+# xx = "albashiiri" # albashiiri is str (string)
+# print(xx)
+
+# z = float(5)
+# print(z)
+
+# """
+
+#   ## Casting
+
+# #example 1
+# a = str(100)
+# b = int(100)
+# c = float(100)
+# print(a, b, c)
+
+# #example 2
+
+# h = 10
+# i = "frozenset"
+# j = 1.3
+# print(type(h))
+# print(type(i))
+# print(type(j))
+
+#  # single or double qoutes
+
+# l = "froz1"
+# k = 'froz2'
+# print(l, k)
+
+#  #Case-Sensitive
+# q = 1
+# Q = 4
+# print(q, Q)
+
+# T = 9
+# T = "sally"
+# print(T, T)
+
+#  #variable names
+
+#  #legal variable names
+# myvar = "Bulbeni"
+# my_var = "Bulbeni"
+# _my_var = "Bulbeni"
+# myVar = "Bulbeni"
+# MYVAR1 = "e"
+# myvar2 = "d"
+# print(myvar2)
+
+#  #illegal variable names
+# """
+# 2myvar = "Bulbeni"
+# my-var = "Bulbeni"
+# my var = "Bulbeni"
+# """
+#  #Multi Words Variable Names
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# #One Value to Multiple Variables
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+# #Unpack a Collection
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+
 """
-##Day 2 24 Jan
-# Python Syntax
-
-print("Hello, Fr oz")
-
-print("djfdf")
-
-# Python Indentation
-
-# problem 1
-if 5 > 2:
-    print("Five is greater than two!")
-
-# problem 2
-if 5 > 2:
- print("Five is greater than two!")
-if 5 > 2:
-        print("Five is greater than two!")
+## 06/02/2023
+### Output Variables
 
 
-# Day 3 26/01/23
-# Comments
-
-print("comment")
-print("comment2") #this is an example of commments
-
-"""
-
-
-
-
-
-##Day 4 27/01/2023
-"""
-# Variables
-
-x = 5 # 5 is int (integer)
+#The Python print() function is often used to output variables.
+x = "Python is awesome"
 print(x)
 
-xx = "albashiiri" # albashiiri is str (string)
-print(xx)
+#In the print() function, you output multiple variables, separated by a comma:
+x = "c++"
+y = "is"
+z = "awesome"
+print(x, y, z)
 
-z = float(5)
-print(z)
+#Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+x = "Python"
+y = "is"
+z = "awesome"
+print(x + y + z) #You can also use the + operator to output multiple variables:
 
+#example
+x = "Machine learning "
+y = "will "
+z = "be awesome"
+print(x + y + z) #You can also use the + operator to output multiple variables:
+
+# In the print() function, when you try to combine a string and a number with the + operator, Python will give you an error: here is example
+x = 5
+y = "Bulbeni"
+#print(x + y) # this is an error
+
+#The best way to output multiple variables in the print() function is to separate them with commas, which even support different data types:
+x = 5
+y = "Bulbeni"
+print(x, y)
 """
 
-  ## Casting
-
-#example 1
-a = str(100)
-b = int(100)
-c = float(100)
-print(a, b, c)
-
-#example 2
-
-h = 10
-i = "frozenset"
-j = 1.3
-print(type(h))
-print(type(i))
-print(type(j))
-
- # single or double qoutes
-
-l = "froz1"
-k = 'froz2'
-print(l, k)
-
- #Case-Sensitive
-q = 1
-Q = 4
-print(q, Q)
-
-T = 9
-T = "sally"
-print(T, T)
-
- #variable names
-
- #legal variable names
-myvar = "John"
-my_var = "John"
-_my_var = "John"
-myVar = "John"
-MYVAR1 = "e"
-myvar2 = "d"
-print(myvar2)
-
- #illegal variable names
-"""
-2myvar = "John"
-my-var = "John"
-my var = "John"
-"""
- #Multi Words Variable Names
+#07/02/2023
 
 
-
-
-##Day 5 30/01/2023
-#finish variable
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Data types
