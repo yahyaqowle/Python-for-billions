917f2b1228feab448e2dd44ccf118fea2ce6498b
Yahya Qowle <79558044+yahyaqowle@users.noreply.github.com>
2023-02-13 10:33:16 +0300

    13/02/2023

--- barasho.py
+++ barasho.py
@@ -1,3 +1,6 @@
+# import random
+# print(random.randint(0, 100))
+
 # """
 # ##Day 2 24 Jan
 # # Python Syntax
@@ -18,7 +21,6 @@
 # if 5 > 2:
 #         print("Five is greater than two!")
 
-
 # # Day 3 26/01/23
 # # Comments
 
@@ -26,10 +28,6 @@
 # print("comment2") #this is an example of commments
 
 # """
-
-
-
-
 
 # ##Day 4 27/01/2023
 # """
@@ -113,9 +111,8 @@
 # print(x)
 # print(y)
 # print(z)
-
-
 """
+
 ## 06/02/2023
 ### Output Variables
 
@@ -151,8 +148,278 @@ y = "Bulbeni"
 x = 5
 y = "Bulbeni"
 print(x, y)
-"""
+
+
 
 #07/02/2023
 
+#Global Variables
 
+  ##Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+
+  #Global variables can be used by everyone, both inside of functions and outside.
+
+x = "aminakoyum"
+
+def myfunc():
+  print("froz " + x)
+
+myfunc()
+#example 2
+
+y = "tusaale"
+
+def froz0():
+  print("sikerim seni " + y)
+
+froz0()
+
+#problem2
+
+#Create a variable inside a function, with the same name as the global variable
+
+W = "albashiiri"
+
+def myfunc():
+  w = "saalah"
+  print("Python is " + w) #If you create a variable with the same name inside a function, this variable will be local, and can only be used inside the function. The global variable with the same name will remain as it was, global and with the original value.
+
+myfunc()
+
+print("Python is " + W)
+
+#The global Keyword
+
+#Normally, when you create a variable inside a function, that variable is local, and can only be used inside that function.
+
+# To create a global variable inside a function, you can use the global keyword.
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+def syntax9():
+  global M
+  M = 7 + 3
+
+syntax9()
+print(M)
+
+
+
+
+
+#Also, use the global keyword if you want to change a global variable inside a function.
+
+x = "casiir"
+
+def myfunc():
+  global x
+  x = "caano"
+
+myfunc()
+
+x = "casiir"
+x = "four"
+print(x)
+ 
+
+
+
+
+# print("hilib " + x) #To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+
+
+
+#09/02/2023
+# Data types
+
+#Built-in Data Types
+#In programming, data type is an important concept. Variables can store data of different types, and different types can do different things.
+
+#python has the following data types built-in by default, in these categories:
+
+x = 5 
+print(x)
+print(type(x))
+
+
+# Text Type:    str
+A = "froz" 
+print(A)
+print(type(A))
+
+
+# Numeric Types:    int, float, complex
+#integer
+B = 5 
+print(B)
+print(type(B))
+
+# float
+C = 5.3
+print(C)
+print(type(C))
+
+#complex
+D = 5j
+print(D)
+print(type(D))
+
+
+# Sequence Types:    list, tuple, range
+#list
+E = ["albashiiri", "froz", "qowle"]
+print(E)
+print(type(E))
+
+#tuple
+F = ("albashiiri", "froz", "qowle") 
+print(F)
+print(type(F))
+
+#range
+H = range(5)
+print(H)
+print(type(H))
+
+# Mapping Type:    dict
+
+I = {"name" : "John", "age" : 36} 
+print(I)
+print(type(I))
+
+
+# Set Types:    set, frozenset
+#set
+J = {"albashiiri", "froz", "qowle"} 
+print(J)
+print(type(J))
+
+#frozenset
+K = frozenset({"albashiiri", "froz", "qowle"}) 
+print(K)
+print(type(K))
+
+# Boolean Type:    bool
+#bool
+L = True
+print(L)
+print(type(L))
+
+# Binary Types:    bytes, bytearray, memoryview
+#bytes
+L = b"Hello qowle"
+print(L)
+print(type(L))
+
+#bytearray
+M = bytearray(7)
+print(M)
+print(type(M))
+
+#memoryview
+N = memoryview(bytes(5))
+print(M)
+print(type(N))
+
+# None Type:    NoneType
+O = None
+print(O)
+print(type(O))
+
+
+# continue 10/02/2023
+
+#Setting the Specific Data Type
+## Text Type:    str
+#str
+a = str("feo<")
+print(a)
+
+#integer
+b = int(9)
+print(b)
+
+c = float(10.5)
+print(c)
+
+d = complex(87j)
+print(d)
+
+e = list(("tufaax", "moos", "cherry"))
+print(e)
+
+f = tuple(("tufaax", "moos", "cherry"))
+print(f)
+
+g = range(5)
+print(g)
+
+h = dict(name="Qowle", age=100)
+print(h)
+
+i = set(("basal", "baradho", "yaanyo"))
+print(i)
+
+j = frozenset(("GAare", "bana", "crry"))
+print(j)
+
+k = bool(32)
+print(k)
+
+l = bytes(60)
+print(l)
+
+m = bytearray(2)
+print(m)
+
+n = memoryview(bytes(15))
+print(n)
+"""
+
+# 13/02/2023
+#Python numbers
+
+#integer
+o = 35656222554887711
+print(o)
+
+#Float
+p  = 9.7639
+print(p)
+
+# Float can also be scientific numbers with an "e" to indicate the power of 10.
+# e qiimaheeda waa 10 yacni, 5e1 = 5 * 10 waanna 50. mar walba e waxa ka dambeeya tirada ey tahay baa zero u xisaabin. tusaale 1e4 waa (1*10000 = 10000)
+#float power
+p2 = 5e8 #500000000
+print(p2)
+
+#complex
+q = 234.5j
+print(q)
+
+#Type Conversion
+#convert from int to float:
+
+r = float(10)
+print(r)
+
+# convert float to complex
+
+s = complex(29.44)
+print(s)
+
+# Random Number
+
+# Python does not have a random() function to make a random number, but Python has a built-in module called random that can be used to make random numbers:
+
+#Import the random module, and display a random number between 1 and 9:
+
+import random
+
+print(random.randrange(5, 18944))
