6f006b960639de956b12ba8a0f662343ba8fa9a1
Yahya Qowle <79558044+yahyaqowle@users.noreply.github.com>
2023-11-30 05:53:56 +0300

    m

--- somaliTTS.py
+++ somaliTTS.py
@@ -1,15 +1,39 @@
-from phonetisaurus import PhonemeGenerator
+from __future__ import division, print_function, absolute_import
+import tflearn
+import speech_data
+import tensorflow as tf
 
-def convert_to_phonemes(word):
-    # Load the pre-trained model or train your own
-    generator = PhonemeGenerator("/home/codert/Documents/programming/model.fst")
+learning_rate = 0.0001
+training_iters = 300000  # steps
+batch_size = 64
 
-    # Convert the word to phonemes
-    phonemes = generator.phonemize(word)
+width = 20  # mfcc features
+height = 80  # (max) length of utterance
+classes = 10  # digits
 
-    return phonemes
+batch = word_batch = speech_data.mfcc_batch_generator(batch_size)
+X, Y = next(batch)
+trainX, trainY = X, Y
+testX, testY = X, Y #overfit for now
 
-# Example usage
-word = "bax"
-phonemes = convert_to_phonemes(word)
-print(f"The phonemes for '{word}' are: {phonemes}")
+# Network building
+net = tflearn.input_data([None, width, height])
+net = tflearn.lstm(net, 128, dropout=0.8)
+net = tflearn.fully_connected(net, classes, activation='softmax')
+net = tflearn.regression(net, optimizer='adam', learning_rate=learning_rate, loss='categorical_crossentropy')
+# Training
+
+### add this "fix" for tensorflow version errors
+col = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+for x in col:
+    tf.add_to_collection(tf.GraphKeys.VARIABLES, x ) 
+
+
+model = tflearn.DNN(net, tensorboard_verbose=0)
+while 1: #training_iters
+  model.fit(trainX, trainY, n_epoch=10, validation_set=(testX, testY), show_metric=True,
+          batch_size=batch_size)
+  _y=model.predict(X)
+model.save("tflearn.lstm.model")
+print (_y)
+print (y)
